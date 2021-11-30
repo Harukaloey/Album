77125c08905dc072a8e360a46f1a909a6454736f
Harukaloey <harukayeol@gmail.com>
2021-11-30 00:07:20 -0600

    Ablum

--- Album.js
+++ Album.js
@@ -1,5 +1,8 @@
 import "./styles.css";
 
+const searchBar = document.getElementById("searchBar");
+const albumList = document.getElementById("albumList");
+
 
   fetch('https://itunes.apple.com/search?term=${ARTIST_NAME}&media=music&entity=album&attribute=artistTerm&limit=50')
    .then(response => response.json())
