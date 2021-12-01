62d7c12e909b1352331e0afccb08e9715dc1ea6a
Harukaloey <harukayeol@gmail.com>
2021-11-30 23:00:23 -0600

    Album

--- Album.js
+++ Album.js
@@ -1,9 +1,27 @@
-import "./styles.css";
 
-const searchBar = document.getElementById("searchBar");
+
+const idBar = document.getElementById("idBar");
+let display = [];
 const albumList = document.getElementById("albumList");
 
+idBar.addEventListener("click", function (e) {
+  e.preventDefault();
 
-  fetch('https://itunes.apple.com/search?term=${ARTIST_NAME}&media=music&entity=album&attribute=artistTerm&limit=50')
-   .then(response => response.json())
-   .then(album => console.log(album.results));
+  fetch(
+    "https://itunes.apple.com/search?term=${ARTIST_NAME}&media=music&entity=album&attribute=artistTerm&limit=200"
+  )
+    .then((res) => res.json())
+    .then((album) => {
+      console.log(album.results);
+      display=album.results.map((item) => {
+        return item.artworkUrl100
+      });
+      for(let i = 0; i < display.length; i++){
+        albumList.innerHTML +=`<li><img src=${display[i]}></img></li>`
+      }
+    });
+});
+
+
+
+
